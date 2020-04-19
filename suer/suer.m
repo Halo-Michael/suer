78e74956c51630fa2f8fb1e03151bbacf31a16b1
dell <yc992147156@me.com>
2020-04-19 23:37:55 +0800

    Update

--- suer/suer.m
+++ suer/suer.m
@@ -8,23 +8,43 @@ int main(int argc, const char **argv, const char **envp) {
     }
 
     if (getuid() != 0 || geteuid() != 0 || getgid() != 0) {
-        NSMutableArray *errors = [NSMutableArray array];
+        NSString *error = @"";
         if (getuid() != 0 || geteuid() != 0){
-            [errors addObject:@"Can't set uid as 0.\n"];
+            error = [error stringByAppendingString:@"Can't set uid as 0.\n"];
         }
         if (getgid() != 0){
-            [errors addObject:@"Can't set gid as 0.\n"];
+            error = [error stringByAppendingString:@"Can't set gid as 0.\n"];
         }
-        NSString *error = [errors componentsJoinedByString:@""];
         printf("%s", [error UTF8String]);
         return 1;
     }
 
-    NSMutableArray *args = [[[NSProcessInfo processInfo] arguments] mutableCopy];
-    [args removeObjectAtIndex:0];
+    NSMutableArray *args = [NSMutableArray array];
+    int i = 1;
+    while (i < argc) {
+        if ([[NSString stringWithFormat:@"%s", argv[i]] containsString:@" "] || [[NSString stringWithFormat:@"%s", argv[i]] containsString:@"$"] || [[NSString stringWithFormat:@"%s", argv[i]] containsString:@"`"]){
+            [args addObject:[NSString stringWithFormat:@"'%s'", argv[i]]];
+        } else if ([[NSString stringWithFormat:@"%s", argv[i]] containsString:@"\""] || [[NSString stringWithFormat:@"%s", argv[i]] containsString:@"'"] || [[NSString stringWithFormat:@"%s", argv[i]] containsString:@"\\"] || [[NSString stringWithFormat:@"%s", argv[i]] containsString:@"*"]){
+            NSString *thisarg = @"";
+            int j = 0;
+            while (j < strlen(argv[i])){
+                if (argv[i][j] == '\"' || argv[i][j] == '\'' || argv[i][j] == '\\' || argv[i][j] == '*'){
+                    thisarg = [thisarg stringByAppendingString:@"\\"];
+                    thisarg = [thisarg stringByAppendingFormat:@"%c", argv[i][j]];
+                } else {
+                    thisarg = [thisarg stringByAppendingFormat:@"%c", argv[i][j]];
+                }
+                j++;
+            }
+            [args addObject:thisarg];
+        } else {
+            [args addObject:[NSString stringWithFormat:@"%s", argv[i]]];
+        }
+        i++;
+    }
+
     NSString *command = [args componentsJoinedByString:@" "];
 
     int ret = system([command UTF8String]);
-
-	return ret;
+    return ret;
 }
